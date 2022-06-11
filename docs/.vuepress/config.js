620ff10b5876d75549a4d9b6311cd29cb0e7cb51
wally-wally <wallys0213@gmail.com>
2022-06-12 01:26:43 +0900

    fix: generateSidebar 파일 제거하고 인라인으로 주입

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,42 @@
 const { description } = require('../../package')
-const { setJSDeepDive, setRefactoring } = require('./generateSidebar')
+
+const padZero = (value) => {
+  return value <= 9 ? `0${value}` : String(value);
+}
+
+const setJSDeepDive = ({ startChapter, endChapter }) => {
+  const overview = {
+    title: 'Overview',
+    path: '/til/js-deep-dive/'
+  }
+
+  const pages = [];
+  for (let chapter = startChapter; chapter <= endChapter; chapter += 1) {
+    pages.push({
+      title: `Chapter${padZero(chapter)}`,
+      path: `/til/js-deep-dive/chapter${padZero(chapter)}`
+    });
+  }
+
+  return [overview, ...pages];
+}
+
+const setRefactoring = ({ startChapter, endChapter }) => {
+  const overview = {
+    title: 'Overview',
+    path: '/til/refactoring/'
+  }
+
+  const pages = [];
+  for (let chapter = startChapter; chapter <= endChapter; chapter += 1) {
+    pages.push({
+      title: `Chapter03-${padZero(chapter)}`,
+      path: `/til/refactoring/chapter03-${padZero(chapter)}`
+    });
+  }
+
+  return [overview, ...pages];
+}
 
 module.exports = {
   /**
